1516cd0a75abd8922c1c7a44a479d78427117ad8
Tristan Beasley <twbeasley13@gmail.com>
2018-07-24 14:04:02 -0400

    enrollments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ApplicationRecord
   has_many :courses
+  has_many :enrollments
+  has_many :enrolled_courses, through: :enrollments, source: :course
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   mount_uploader :image, ImageUploader
+  def enrolled_in?(course))
+   return enrolled_courses.include?(course)
+  end
+  
+  
+  
 end
